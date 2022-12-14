cd8ec8e14eeac624891aab919af4a625b672117e
amanprajapat08278 <amanprajapat08278@gmail.com>
2022-12-14 16:39:51 +0530

    done

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,9 +1,14 @@
 const urlModel = require("../model/urlModel")
 let shortId = require("shortid")
 const validUrl = require('valid-url')
-const urlExists = require("url-exists");
 let axios = require("axios")
+const redis = require("redis");
+const { promisify } = require("util")
 
+
+
+
+//-----------------Create Short URL API--------------------//
 
 const creatUrl = async (req, res) => {
 
@@ -12,14 +17,14 @@ const creatUrl = async (req, res) => {
         if (Object.keys(data).length == 0) { return res.status(400).send({ status: false, message: "Please enter URL" }) }
 
         let longUrl = data.longUrl.trim()
-        if (typeof(longUrl) != "string" || !validUrl.isUri(longUrl)) { return res.status(400).send({ status: false, message: "Please enter valid URL" }) }
+        if (typeof (longUrl) != "string" || !validUrl.isUri(longUrl)) { return res.status(400).send({ status: false, message: "Please enter valid URL" }) }
 
         let axiosData = await axios.get(longUrl).catch(() => null)
-       
-        if (!axiosData) { return res.status(404).send({ status: false, message: `Error! Link Not Found ${longUrl}`}) }
+
+        if (!axiosData) { return res.status(404).send({ status: false, message: `Error! Link Not Found ${longUrl}` }) }
 
         let doxByUrl = await urlModel.findOne({ longUrl: longUrl }).select({ __v: 0, _id: 0 })
-        if (doxByUrl) { return res.status(200).send({ data: doxByUrl })}
+        if (doxByUrl) { return res.status(200).send({ data: doxByUrl }) }
 
         let urlCode = shortId.generate()
         urlCode = (urlCode.toLowerCase()).trim()
@@ -39,14 +44,42 @@ const creatUrl = async (req, res) => {
 
 
 
+//----------------- Radis functions --------------------//
+
+const redisClient = redis.createClient(13190, "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com", { no_ready_check: true });
+
+redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", (err) => {
+    if (err) throw err;
+});
+
+redisClient.on("connect", async () => {
+    console.log("redis connnected")
+})
+
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
+
+
+//----------------- Get Short URL API --------------------//
+
 const getUrl = async (req, res) => {
     let urlCode = req.params.urlCode
     if (!shortId.isValid(urlCode)) { return res.status(400).send({ status: false, message: "Please enter valid url Code" }) }
 
-    let result = await urlModel.findOne({ urlCode: urlCode })
-    if (!result) { return res.status(404).send({ status: false, message: "URL not found" }) }
+    let dataByRadis = await GET_ASYNC(`${urlCode}`)
 
-    return res.status(302).redirect(result.longUrl)
+    if (dataByRadis) {
+        dataByRadis = JSON.parse(dataByRadis)
+        return res.status(302).redirect(dataByRadis.longUrl)
+    }
+    else {
+        let result = await urlModel.findOne({ urlCode: urlCode })
+        if (!result) { return res.status(404).send({ status: false, message: "URL not found" }) }
+        await SET_ASYNC(`${urlCode}`, JSON.stringify(result))
+        return res.status(302).redirect(result.longUrl)
+    }
 }
 
 
--- z.js
+++ z.js
@@ -1,15 +1,35 @@
-// var IsAlive = require('is-alive');
-// var isAlive = new IsAlive();
-// isAlive.add("http://google.com", 301, function (err) {
-//     "use strict";
 
-//     if (err) {
-//         console.log(err);
-//     }
-// });
 
-// setInterval(function () {
-//     console.log(isAlive.isAlive("http://google.com"));
-// }, 2000);
+let data = [];
+let size = data.length;
+let max = 5;
 
-// console.log(isAlive.isAlive("http://google.com"));
+function push(value){
+    if(size >=max){
+        return console.log("I am full");
+    };
+    data[size] = value;
+    size+=1                     //[1,2,3,4,5,6]
+}
+
+function pop(){
+    if(size <= 0){
+        return console.log("I am alredy empty")
+    }
+    size-=1
+    data.length = size
+}
+
+function display(){
+    console.log(data)
+}
+ push(30)
+ push(30)
+ push(90)
+ push(50)
+ push(60)
+ 
+ pop()
+ pop()
+ 
+ display()
