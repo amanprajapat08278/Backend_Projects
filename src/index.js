8bcbce0aff0f7b47b8fd6967baf90acf14cabd56
amanprajapat08278 <amanprajapat08278@gmail.com>
2023-02-01 21:13:02 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const route = require("./route/route")
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.json())
+app.use(cors({ origin: "*" }))
 
 app.use("/", route)
 
-mongoose.set({"strictQuery":false})
-mongoose.connect("mongodb+srv://amanprajapat82780:Lucky82780@newproject.3qdy8y3.mongodb.net/url_shortner?retryWrites=true&w=majority",{
-    useNewUrlParser:true
-}).then(()=>console.log("Mongoose Connected"))
-.catch((err)=>console.log(err))
+mongoose.set({ "strictQuery": false })
 
-app.listen((process.env.PORT||3000), ()=>{
-    console.log("server running on ", (process.env.PORT||3000))
+mongoose.connect("mongodb+srv://amanprajapat82780:Lucky82780@newproject.3qdy8y3.mongodb.net/url_shortner?retryWrites=true&w=majority", { useNewUrlParser: true })
+    .then(() => console.log("Mongoose Connected"))
+    .catch((err) => console.log(err))
+
+app.listen((process.env.PORT || 3000), () => {
+    console.log("server running on ", (process.env.PORT || 3000))
 })
